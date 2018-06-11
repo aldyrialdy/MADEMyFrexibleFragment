9e3091807e57c9e1b86f665458c21ce2f21b66c9
aldyrialdy <aldy@if.uinsgd.ac.id>
2018-06-11 13:55:48 +0700

    Project latihan Fragment

---
+++ app/src/main/java/com/dicoding/associate/mademyfrexiblefragment/DetailCategoryFragment.java
@@ -0,0 +1,81 @@
+package com.dicoding.associate.mademyfrexiblefragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailCategoryFragment extends Fragment implements View.OnClickListener {
+    private TextView tvCategoryName;
+    private TextView tvCategoryDescription;
+    private Button btnProfile;
+    private Button btnShowDialog;
+    private String description;
+    public static String EXTRA_NAME = "extra_name";
+
+
+    public DetailCategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_detail_category, container, false);
+        tvCategoryName = view.findViewById(R.id.tv_category_description);
+        tvCategoryDescription = view.findViewById(R.id.tv_category_name);
+        btnProfile = view.findViewById(R.id.btn_profile);
+        btnShowDialog = view.findViewById(R.id.btn_show_dialog);
+        btnProfile.setOnClickListener(this);
+        btnShowDialog.setOnClickListener(this);
+        return view;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_profile:
+                break;
+            case R.id.btn_show_dialog:
+                OptionDialogFragment mOptionDialogFragment = new OptionDialogFragment();
+                mOptionDialogFragment.setOnOptionDialogListener(new OptionDialogFragment.OnOptionDialogListener() {
+                    @Override
+                    public void onOptionChoosen(String text) {
+                        Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                FragmentManager mFragmentManager = getChildFragmentManager();
+                mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment.class.getSimpleName());
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String categoryName = getArguments().getString(EXTRA_NAME);
+        tvCategoryName.setText(categoryName);
+        tvCategoryDescription.setText(getDescription());
+    }
+}
